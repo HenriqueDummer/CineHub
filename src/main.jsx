505f4ddc3bdab7ec247221b4e53ceff11a0a2b46
Henrique Dummer <hatiehique2005@gmail.com>
2025-10-29 13:55:00 -0300

    Imported query client

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 
