a83907c36f0f24b5d049f840f1b9f1717bdc19af
Henrique Dummer <hatiehique2005@gmail.com>
2025-10-29 13:40:03 -0300

    Added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import Catalog from "./pages/Catalog"
+import Search from "./pages/Search"
+import Details from "./pages/Details"
 
 function App() {
   return (
     <Routes>
-      
+      {/* Home page */}
+      <Route path="/" element={<Home />}  />
+
+      {/* Movies page */}
+      <Route path="/movies" element={<Catalog />}  />
+
+      {/* Series page */}
+      <Route path="/series" element={<Catalog />}  />
+
+      {/* Search page */}
+      <Route path="/search" element={<Search />}  />
+
+      {/* Movie/Series details page */}
+      <Route path="/details/:id" element={<Details />}  />
     </Routes>
   )
 }
